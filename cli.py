677ae288a8af7862242a367ab32b2faf7aef7246
Jonathan Metz <jonathan.metz@invitae.com>
2020-09-25 00:27:42 -0700

    Add search functionality

--- cli.py
+++ cli.py
@@ -1,11 +1,11 @@
 import argparse
-import random
 
-from helpers import get_athletes
+from helpers import get_random_athlete
 
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Get a random athlete")
+    parser.add_argument("-s", "--search", help="Search by name")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-q", "--quiet", action="store_true", help="print quiet")
     group.add_argument("-v", "--verbose", action="store_true", help="print verbose")
@@ -15,14 +15,10 @@ def get_parser() -> argparse.ArgumentParser:
 def main() -> None:
     parser = get_parser()
     args = parser.parse_args()
-    athletes = get_athletes("athletes", verbose=args.verbose)
-    athlete = random.choice(athletes)
-    if args.quiet:
-        print(athlete.name)
-    else:
-        print(
-            f"{athlete.name} is {athlete.height_display} and {athlete.age} years old."
-        )
+    random_athlete = get_random_athlete(
+        search=args.search, quiet=args.quiet, verbose=args.verbose
+    )
+    print(random_athlete if random_athlete else "No athletes found")
 
 
 if __name__ == "__main__":
